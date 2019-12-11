4c0aa988cb4ce702b2e448fb473414364e41f16f
DanielMora12 <56009611+DanielMora12@users.noreply.github.com>
2019-12-10 18:34:47 -0600

    Create prueba.java

---
+++ prueba.java
@@ -0,0 +1,20 @@
+public class prueba {
+
+    public static void main(String [] args){
+    	int a;
+		int b;
+		
+    	System.out.println("Capture el primer valor(Entero)");
+    	
+    	System.out.println("Capture el segundo numero(Decimal)");
+		 int suma= a+b;
+		 int resta = a-b;
+    	if (suma >= 30) {
+    		System.out.println("El resultado de la suma es grande" + suma);
+    		System.out.println("Y la resta es :" + resta);
+    		}else {
+    			System.out.println("El resultado de la suma es pequeña" + suma);
+    			System.out.println("Y la resta es :" + resta);
+    		}	
+    }
+}
